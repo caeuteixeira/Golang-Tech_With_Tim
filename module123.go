110e8cca040860b79f017e807d322b0f4b888e7f
Caeu <caeufertte@gmail.com>
2023-07-30 23:10:01 -0300

    First steps on Golang

---
+++ module123.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var number uint16 = 260
+	/* var number = 260 and number := 260 can be used as well */
+	number = number + 5
+	fmt.Println("Hello, world!", number)
+}
